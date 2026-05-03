d751c8dbfb2359c081429c49f8bf9a7e8a39fd7d
syedasaniyaa508 <saniyasyed508@gmail.com>
2026-05-03 11:10:09 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+int main() {
+    char name[50];
+    int roll;
+    float marks;
+
+    printf("Enter student name: ");
+    scanf("%s", name);
+
+    printf("Enter roll number: ");
+    scanf("%d", &roll);
+
+    printf("Enter marks: ");
+    scanf("%f", &marks);
+
+    printf("\n----- STUDENT RECORD -----\n");
+    printf("Name: %s\n", name);
+    printf("Roll Number: %d\n", roll);
+    printf("Marks: %.2f\n", marks);
+
     return 0;
 }
